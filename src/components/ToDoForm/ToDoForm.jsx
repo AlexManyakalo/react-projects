37e84a9dc747f61a8843b290f4b15179867876da
Alex06052003 <amm-2003@mail.ru>
2025-03-03 20:43:38 +0800

    feat: add components

--- src/components/ToDoForm/ToDoForm.jsx
+++ src/components/ToDoForm/ToDoForm.jsx
@@ -1,11 +1,26 @@
-import "./ToDoForm.scss";
+import styles from "./ToDoForm.module.scss";
 
-function ToDoForm() {
+function ToDoForm(props) {
+  const { newTaskText, addTask, setNewTaskText } = props;
+
   return (
-  <>
-    
-  </>
-);
+    <>
+      <div className="todo__form">
+        <h2 className="form__title">Add task</h2>
+        <div className={styles.form__controls}>
+          <input
+            type="text"
+            className="form__controls-input"
+            value={newTaskText}
+            onChange={e => setNewTaskText(e.target.value)}
+          />
+          <button className="form__controls-btn" onClick={() => addTask()}>
+            Add
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ToDoForm;
--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -1,7 +1,18 @@
-import "./ToDoItem.scss";
+import styles from "./ToDoItem.module.scss";
 
-function ToDoItem() {
-  return <></>;
+function ToDoItem(props) {
+  const { id, text, deleteTask } = props;
+
+  return (
+    <>
+      <li className={styles.todo__item}>
+        <p className="todo__item-paragraph">
+          {id}. {text}
+        </p>
+        <button className="todo__item-btn" onClick={() => deleteTask(id)}>X</button>
+      </li>
+    </>
+  );
 }
 
 export default ToDoItem;
