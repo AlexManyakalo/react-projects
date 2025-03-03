f0e46c3bb9c3d6368e1ebbcc9bd76ed21e56b2e8
Alex06052003 <amm-2003@mail.ru>
2025-03-03 20:43:48 +0800

    feat: add main app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,62 @@
-import { styles } from "./App.module.scss";
+import { useState } from "react";
+import ToDoItem from "./components/ToDoItem/ToDoItem.jsx";
+import ToDoForm from "./components/ToDoForm/ToDoForm.jsx";
+import styles from "./App.module.scss";
 
 function App() {
+  const [newTaskText, setNewTaskText] = useState("");
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "rgergerger",
+    },
+    {
+      id: 2,
+      text: "thrthrt",
+    },
+    {
+      id: 3,
+      text: "wrttjetyj",
+    },
+  ]);
+
+  function handleDeleteTask(id) {
+    setTasks(tasks.filter(task => task.id !== id));
+  }
+
+  function handleAddTask() {
+    if (newTaskText.trim() === "") return;
+    const newTask = {
+      id: Date.now(),
+      text: newTaskText,
+    };
+    setTasks([...tasks, newTask]);
+    setNewTaskText("");
+  }
+
   return (
     <>
-      <div className="container">thrthrth</div>
+      <div className={`${styles.todo} container`}>
+        <h1 className="todo__title todo__title--big">ToDoList</h1>
+        <ToDoForm
+          newTaskText={newTaskText}
+          addTask={handleAddTask}
+          setNewTaskText={setNewTaskText}
+        />
+        <div className="todo__form">
+          <h2 className="todo__title">Tasks</h2>
+          <ul className={styles.todo__list}>
+            {tasks.map(task => (
+              <ToDoItem
+                key={task.id}
+                id={task.id}
+                text={task.text}
+                deleteTask={handleDeleteTask}
+              ></ToDoItem>
+            ))}
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
