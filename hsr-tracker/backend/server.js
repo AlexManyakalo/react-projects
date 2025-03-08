7d397655d85c7f6880c75ed512d286856b32fb61
Alex06052003 <amm-2003@mail.ru>
2025-03-08 19:31:24 +0800

    feat: delete excess point

--- hsr-tracker/backend/server.js
+++ hsr-tracker/backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";
+import api from "./api.js";
 import cors from "cors";
+
 const app = express();
 const port = 3000;
 
@@ -11,23 +13,16 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/movies/recommend", async (req, res) => {
-  const data = await getMovies();
-  res.send(data);
-});
+/*================== ROUTES ==================*/
 
-app.get("/movie/genres", async (req, res) => {
-  const data = await getMovieGenres(id);
-  res.send(data);
-});
+app.post("/player", async (req, res) => {
+  const { uid } = req.body;
 
-// Страница фильма
-app.get("/movie/:id", async (req, res) => {
-  const id = +req.params.id;
-  const movies = await getMovies();
-
-  const movie = movies.find(movie => movie.id === id);
-
-  if (movie) res.send(movie);
-  else res.status(404).send({ error: "Фильм не найден" });
+  if (!uid) return res.status(400).json({ error: "UID не передан" });
+  try {
+    const response = await api.apiServer.get(`sr_info_parsed/${uid}?language=ru`);
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Ошибка при получении данных" });
+  }
 });
