a875b455897b6b540a0a9210accf24698af9f99d
Alex06052003 <amm-2003@mail.ru>
2025-03-08 19:33:17 +0800

    feat: add form component

---
+++ hsr-tracker/src/components/Form/Form.jsx
@@ -0,0 +1,27 @@
+import styles from "./Form.module.scss";
+
+function Form(props) {
+  const { uid, setUid, handleSubmit } = props;
+
+  return (
+    <section className={styles.data}>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+        className={styles.data__form}
+      >
+        <input
+          type="text"
+          value={uid}
+          onChange={e => setUid(e.target.value)}
+          placeholder="Введите UID"
+        />
+        <button type="submit">Найти</button>
+      </form>
+    </section>
+  );
+}
+
+export default Form;
