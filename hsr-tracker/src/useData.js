48c0642131f10f2f992818d345d92ec45690114d
Alex06052003 <amm-2003@mail.ru>
2025-03-10 18:17:55 +0800

    feat: add loading

--- hsr-tracker/src/useData.js
+++ hsr-tracker/src/useData.js
@@ -1,13 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import api from "../backend/api";
 
 export function useData() {
   const [uid, setUid] = useState("");
   const [playerData, setPlayerData] = useState({});
   const [charactersData, setCharactersData] = useState([]);
-  const [errorMessage, setErrorMessage] = useState("");
+  const [selectedCharacter, setSelectedCharacter] = useState(null);
 
   const UID_LENGTH = 9;
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   async function handleSubmit() {
     if (!uid || uid.length !== UID_LENGTH || !/^\d+$/.test(uid)) {
@@ -15,8 +18,8 @@ export function useData() {
       return;
     }
     try {
+      setIsLoading(true);
       const res = await api.apiClient.post("/player", { uid });
-
       setPlayerData(res.data.player);
       setCharactersData(res.data.characters);
       // console.debug("Ответ сервера:", res.data);
@@ -26,6 +29,12 @@ export function useData() {
       setErrorMessage("Ошибка отправки данных");
     }
   }
+
+  useEffect(() => {
+    if (charactersData.length > 0) {
+      setIsLoading(false);
+    }
+  }, [charactersData]);
 
   return {
     uid,
@@ -37,5 +46,9 @@ export function useData() {
     errorMessage,
     setErrorMessage,
     handleSubmit,
+    isLoading,
+    setIsLoading,
+    selectedCharacter,
+    setSelectedCharacter,
   };
 }
