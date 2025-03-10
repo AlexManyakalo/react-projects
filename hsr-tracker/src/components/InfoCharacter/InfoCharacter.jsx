3f7797a6924e22e829de308f4a252f96f81d0fa2
Alex06052003 <amm-2003@mail.ru>
2025-03-10 18:20:02 +0800

    feat: create InfoCharacter component

--- hsr-tracker/src/components/InfoCharacter/InfoCharacter.jsx
+++ hsr-tracker/src/components/InfoCharacter/InfoCharacter.jsx
@@ -1,8 +1,26 @@
+import api from "../../../backend/api.js";
 import styles from "./InfoCharacter.module.scss";
 
-function InfoCharacter() {
+function InfoCharacter(props) {
+  const { character, onClose } = props;
 
   return (
+    <>
+      <div className={styles.popup} onClick={onClose}></div>
+      <section className={styles.info}>
+        <div className="info__top">
+          <div className="info__top-left">
+            <img
+              className="info__top-image"
+              src={`${api.apiImages.defaults.baseURL}${character.portrait}`}
+              alt={`Персонаж ${character.name}`}
+            />
+          </div>
+          <div className="info__top-right"></div>
+        </div>
+        <div className="info__bottom"></div>
+      </section>
+    </>
   );
 }
 
