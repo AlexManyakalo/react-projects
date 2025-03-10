caa4d777551e8831c8be89c7f729ed557bbcf0ca
Alex06052003 <amm-2003@mail.ru>
2025-03-10 18:17:14 +0800

    feat: add new components

--- hsr-tracker/src/App.jsx
+++ hsr-tracker/src/App.jsx
@@ -1,5 +1,9 @@
 import Form from "./components/Form/Form.jsx";
 import ErrorWindow from "./components/ErrorWindow/ErrorWindow.jsx";
+import AllCharacters from "./components/AllCharacters/AllCharacters.jsx";
+import InfoCharacter from "./components/InfoCharacter/InfoCharacter.jsx";
+import Loader from "./components/Loader/Loader.jsx";
+import styles from "./App.module.scss";
 import { useData } from "./useData.js";
 
 function App() {
@@ -11,17 +15,43 @@ function App() {
     errorMessage,
     setErrorMessage,
     handleSubmit,
+    isLoading,
+    setIsLoading,
+    selectedCharacter,
+    setSelectedCharacter,
   } = useData();
 
   return (
     <div className="container">
-      <Form uid={uid} setUid={setUid} handleSubmit={handleSubmit} />
-      {errorMessage && (
-        <ErrorWindow
-          errorMessage={errorMessage}
-          setErrorMessage={setErrorMessage}
-        />
-      )}
+      <main className={styles.content}>
+        <Form uid={uid} setUid={setUid} handleSubmit={handleSubmit} />
+        <ul className={styles.content__list}>
+          {isLoading ? (
+            <Loader />
+          ) : (
+            charactersData.map(character => (
+              <AllCharacters
+                key={character.id}
+                character={character}
+                onClick={() => setSelectedCharacter(character)}
+              />
+            ))
+          )}
+        </ul>
+        {selectedCharacter && (
+          <InfoCharacter
+            character={selectedCharacter}
+            onClose={() => setSelectedCharacter(null)}
+          />
+        )}
+        {errorMessage && (
+          <ErrorWindow
+            errorMessage={errorMessage}
+            setErrorMessage={setErrorMessage}
+            setIsLoading={setIsLoading}
+          />
+        )}
+      </main>
     </div>
   );
 }
